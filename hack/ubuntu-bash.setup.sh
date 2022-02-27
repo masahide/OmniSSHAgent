7f40e9adf4d9db84011e2bdd292701b5dc2fe67d
YAMASAKI Masahide <masahide.y@gmail.com>
2022-02-27 23:00:46 +0900

    fix

--- hack/ubuntu-bash.setup.sh
+++ hack/ubuntu-bash.setup.sh
@@ -1,28 +1,31 @@
-#!/bin/bash
 
-OMNISOCAT=$HOME/omni-socat/omni-socat.exe
-URL=https://github.com/masahide/OmniSSHAgent/releases/latest/download/omni-socat.zip
-
-get_omnisocat () {
-    echo "Get omni-socat.exe"
-    curl -sL $URL -o omni-socat.zip
-    unzip -o omni-socat.zip -d $(dirname $OMNISOCAT)
-    rm omni-socat.zip
-}
-
-get_socat () {
-    echo "Install socat"
-    sudo apt -y install socat
-}
-
-[[ -f $OMNISOCAT ]] || get_omnisocat
-[[ -f /usr/bin/socat ]] || get_socat
-
+OMNISOCATCMD=$HOME/omni-socat/omni-socat.exe
 export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
 
-ss -a | grep -q $SSH_AUTH_SOCK
-if [[ $? -ne 0 ]]; then
-    rm -f $SSH_AUTH_SOCK
-    (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$OMNISOCAT",nofork &) >/dev/null 2>&1
-fi
+__get_omnisocat () {
+  echo "Get omni-socat.exe"
+  curl https://github.com/masahide/OmniSSHAgent/releases/latest/download/omni-socat.zip \
+      -sLo omni-socat.zip
+  unzip -o omni-socat.zip -d $(dirname $OMNISOCATCMD)
+  rm omni-socat.zip
+}
+
+__get_socat () {
+  echo "Install socat"
+  sudo apt -y install socat
+}
+
+
+setup_omnisocat () {
+  [[ -f $OMNISOCATCMD ]]  || __get_omnisocat
+  [[ -f /usr/bin/socat ]] || __get_socat
+  
+  ss -a | grep -q $SSH_AUTH_SOCK
+  [[ $? -ne 0 ]]  || return
+
+  rm -f $SSH_AUTH_SOCK
+  (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$OMNISOCATCMD",nofork &) >/dev/null 2>&1
+}
+
+setup_omnisocat
 
