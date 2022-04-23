cb0327a35f187594f5b70408b07a64d4c6a7a6c2
Maicol Battistini <maicolbattistini@live.it>
2022-04-23 12:45:31 +0200

    Create ubuntu-fish.setup.fish

---
+++ hack/ubuntu-fish.setup.fish
@@ -0,0 +1,36 @@
+set OMNISOCATCMD $HOME/omni-socat/omni-socat.exe
+export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
+
+function __get_omnisocat
+  echo "Get omni-socat.exe"
+  curl https://github.com/masahide/OmniSSHAgent/releases/latest/download/omni-socat.zip \
+      -sLo omni-socat.zip
+  unzip -o omni-socat.zip -d (dirname $OMNISOCATCMD)
+  rm omni-socat.zip
+end
+
+function __get_socat
+  echo "Install socat"
+  sudo apt -y install socat
+end
+
+
+function setup_omnisocat
+  if not test -f $OMNISOCATCMD
+    __get_omnisocat
+  end
+  if not test -f /usr/bin/socat
+    __get_socat
+  end
+  
+  ss -a | grep -q $SSH_AUTH_SOCK
+  if not test $status -ne 0
+    return
+  end
+  
+  rm -f $SSH_AUTH_SOCK
+
+  setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$HOME/omni-socat/omni-socat.exe" >/dev/null 2>&1 & disown
+end
+
+setup_omnisocat
