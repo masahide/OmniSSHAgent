08e79ec16ca550379bb671ee648a3c7e8e508d40
Ruben Anders <ruben.anders@robotty.de>
2024-01-08 01:46:05 +0100

    Create .ssh directory automatically
    This commit also makes the script POSIX compliant (at least I hope so, I validated the script with shellcheck.)
    
    Also turned the `-f` checks that had hardcoded install paths into the universally available `command` check, which will work regardless of where the system utilities are installed.

---
+++ hack/rocky.setup.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+OMNISOCATCMD="$HOME/omni-socat/omni-socat.exe"
+export SSH_AUTH_SOCK="$HOME/.ssh/agent.sock"
+
+__get_omnisocat () {
+  echo "Downloading omni-socat.exe"
+  curl https://github.com/masahide/OmniSSHAgent/releases/latest/download/omni-socat.zip \
+      -sLo omni-socat.zip
+  unzip -o omni-socat.zip -d "$(dirname "$OMNISOCATCMD")"
+  chmod +x "$OMNISOCATCMD"
+  rm -f omni-socat.zip
+}
+
+__get_ss ()  {
+  echo "Installing ss"
+  sudo dnf -y install iproute
+}
+
+__get_socat () {
+  echo "Installing socat"
+  sudo dnf -y install socat
+}
+
+setup_omnisocat () {
+  [ -f "$OMNISOCATCMD" ] || __get_omnisocat
+  command -v ss >/dev/null 2>&1 || __get_ss
+  command -v socat >/dev/null 2>&1 || __get_socat
+
+  # Checks wether $SSH_AUTH_SOCK is a socket or not
+  (ss -a | grep -q "$SSH_AUTH_SOCK") && [ -S "$SSH_AUTH_SOCK" ] && return
+
+  # Create directory for the socket, if it is missing
+  SSH_AUTH_SOCK_DIR="$(dirname "$SSH_AUTH_SOCK")"
+  mkdir -p "$SSH_AUTH_SOCK_DIR"
+  # Applying best-practice permissions if we are creating $HOME/.ssh
+  if [ "$SSH_AUTH_SOCK_DIR" = "$HOME/.ssh" ]; then
+    chmod 700 "$SSH_AUTH_SOCK_DIR"
+  fi
+
+  rm -f "$SSH_AUTH_SOCK"
+  (setsid socat UNIX-LISTEN:"$SSH_AUTH_SOCK",fork EXEC:"$OMNISOCATCMD",nofork &) >/dev/null 2>&1
+}
+
+setup_omnisocat
--- hack/ubuntu-fish.setup.fish
+++ hack/ubuntu-fish.setup.fish
@@ -22,7 +22,7 @@ function setup_omnisocat
   if not test -f $OMNISOCATCMD
     __get_omnisocat
   end
-  if not test -f /usr/bin/socat
+  if not command -v socat >/dev/null 2>&1
     __get_socat
   end
   
@@ -30,6 +30,15 @@ function setup_omnisocat
   ss -a | grep -q $SSH_AUTH_SOCK
   if test -S $SSH_AUTH_SOCK -a $status -eq 0
     return
+  end
+
+  # Create directory for the socket, if it is missing
+  set SSH_AUTH_SOCK_DIR (dirname $SSH_AUTH_SOCK)
+  mkdir -p $SSH_AUTH_SOCK_DIR
+
+  # Applying best-practice permissions if we are creating $HOME/.ssh
+  if test "$SSH_AUTH_SOCK_DIR" = "$HOME/.ssh"
+      chmod 700 $SSH_AUTH_SOCK_DIR
   end
   
   rm -f $SSH_AUTH_SOCK
