0810495936c3a6f4fbabcbe38dad30d127b1508a
YAMASAKI Masahide <masahide.y@gmail.com>
2024-01-08 15:01:05 +0900

    add

--- hack/rocky.setup.sh
+++ hack/rocky.setup.sh
@@ -3,29 +3,29 @@
 OMNISOCATCMD="$HOME/omni-socat/omni-socat.exe"
 export SSH_AUTH_SOCK="$HOME/.ssh/agent.sock"
 
-__get_omnisocat () {
+__get_omnisocat() {
   echo "Downloading omni-socat.exe"
   curl https://github.com/masahide/OmniSSHAgent/releases/latest/download/omni-socat.zip \
-      -sLo omni-socat.zip
+    -sLo omni-socat.zip
   unzip -o omni-socat.zip -d "$(dirname "$OMNISOCATCMD")"
   chmod +x "$OMNISOCATCMD"
   rm -f omni-socat.zip
 }
 
-__get_ss ()  {
+__get_ss() {
   echo "Installing ss"
   sudo dnf -y install iproute
 }
 
-__get_socat () {
+__get_socat() {
   echo "Installing socat"
   sudo dnf -y install socat
 }
 
-setup_omnisocat () {
+setup_omnisocat() {
   [ -f "$OMNISOCATCMD" ] || __get_omnisocat
-  command -v ss >/dev/null 2>&1 || __get_ss
-  command -v socat >/dev/null 2>&1 || __get_socat
+  command -v ss > /dev/null 2>&1 || __get_ss
+  command -v socat > /dev/null 2>&1 || __get_socat
 
   # Checks wether $SSH_AUTH_SOCK is a socket or not
   (ss -a | grep -q "$SSH_AUTH_SOCK") && [ -S "$SSH_AUTH_SOCK" ] && return
@@ -39,7 +39,7 @@ setup_omnisocat () {
   fi
 
   rm -f "$SSH_AUTH_SOCK"
-  (setsid socat UNIX-LISTEN:"$SSH_AUTH_SOCK",fork EXEC:"$OMNISOCATCMD",nofork &) >/dev/null 2>&1
+  (setsid socat UNIX-LISTEN:"$SSH_AUTH_SOCK",fork EXEC:"$OMNISOCATCMD",nofork &) > /dev/null 2>&1
 }
 
 setup_omnisocat
